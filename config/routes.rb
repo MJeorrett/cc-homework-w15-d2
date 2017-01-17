92ec92e19eb887ea18f41c6497769bdd9e106b97
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-17 15:54:35 +0000

    adding index and show routes for Favourite

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :shows, only: [ :index, :show ]
   resources :users, only: [ :index, :show ]
+  resources :favourites, only: [ :index, :show ]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
