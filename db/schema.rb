06c950c4f8fc480bce48497bc468afbafcffaeed
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-17 15:43:49 +0000

    migrating db to version including user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117152156) do
+ActiveRecord::Schema.define(version: 20170117153409) do
 
   create_table "shows", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20170117152156) do
     t.string   "programme_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
